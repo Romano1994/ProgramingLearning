03d014ddddcac562ab4fb82df37b4f829e4dc8d2
Romano1994 <109515270+Romano1994@users.noreply.github.com>
2022-07-27 23:28:59 +0900

    Add files via upload

---
+++ Ch4_pratices/Ch4_1.java
@@ -0,0 +1,13 @@
+/**
+ * 4_1
+ */
+public class Ch4_1{
+
+    public static void main(String[] args) {
+        int score = 80;
+
+        if (score >= 70) {
+            System.out.println("합격입니다");
+        }
+    }
+}
