0479dd1c620346afa2944fb4da6a0c37494024c4
Romano1994 <109515270+Romano1994@users.noreply.github.com>
2022-09-12 17:10:39 +0900

    Add files via upload

--- Date9_10/src/Ch9pr_6.java
+++ Date9_10/src/Ch9pr_6.java
@@ -1,4 +1,6 @@
+import java.nio.Buffer;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,9 +12,9 @@ import java.util.regex.Pattern;
 public class Ch9pr_6 {
 
 	public static void main(String[] args) {
-		String[] phoneNumber = {	//저장된 전화번호 리스트. 입력된 전화번호의 일부가 해당 배열에 있으면 찾아서 출력함 
-				"010-3456-7890",
-				"099-2456-7890",
+		String[] phoneNumberArr = {	//저장된 전화번호 리스트. 입력된 전화번호의 일부가 해당 배열에 있으면 찾아서 출력함 
+				"012-3456-7890",
+				"099-2456-7980",
 				"088-2346-9870",
 				"013-3456-7890"
 		};
@@ -29,18 +31,28 @@ public class Ch9pr_6 {
 			}else if (input.equalsIgnoreCase("Q")) {	// String.equalsIgnoreCase는 String에 해당 문자가 포함되어 있는지 대소문자 구분 없이 비교 
 				System.exit(0);	// 무조건 종료 
 			}
-			for(int i = 1; i < phoneNumber.length; i++) {
-				Pattern regex = Pattern.compile(phoneNumber[i]);
-				if(regex.matches(phoneNumber[i], input)) {
-					list.add(input);
+			String regex = ".*"+input+".*";
+			Pattern pattern = Pattern.compile(regex);
+			
+			for(int i = 0; i < phoneNumberArr.length; i++) {
+				String phoneNumber = phoneNumberArr[i];
+				String tmp = phoneNumber.replace("-", "");
+				
+				Matcher matcher = pattern.matcher(tmp);
+				if(matcher.find()) {
+					list.add(phoneNumber);
 				}
 			}
-			/*
-			
-			식을 완성하시오	
-			
-			
-			*/
+//			Pattern pattern = Pattern.compile("^[0-9]*$");	//비교에 이용할 정규식 저장 
+//			Matcher matcher = pattern.matcher(input); //비교에 이용할 input값을 저장 
+//			matcher.find();
+//			if(matcher.matches()) {
+//				for(int i = 0; i < phoneNumber.length; i++) {
+//					if(phoneNumber[i].indexOf(matcher.group()) != -1) {
+//						list.add(phoneNumber[i]);
+//					}
+//				}
+//			}
 			if(list.size() > 0) {
 				System.out.println(list);
 				list.clear();
